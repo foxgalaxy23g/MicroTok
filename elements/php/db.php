f9b4f932e70ba1bf8330e0123f85b60f95621b89
foxgalaxy23g <gamernext2@gmail.com>
2025-02-06 17:18:06 +0300

    Add files via upload

--- elements/php/db.php
+++ elements/php/db.php
@@ -13,19 +13,23 @@ $registration_open = "1";
 ini_set('display_errors', 13);
 error_reporting(E_ALL);
 
-// Connect to database
-$conn = new mysqli($host, $user, $pass, $db);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-if ($conn->connect_error) {
-    $server_open_now = "0"; 
-    if (basename($_SERVER['PHP_SELF']) !== 'closed.php') {
-        header('Location: closed.php');
-        exit();
-    }
-    die();
+// Изначально считаем, что сервер работает
+$server_open_now = 1;
+
+try {
+    $conn = new mysqli($host, $user, $pass, $db);
+} catch (mysqli_sql_exception $e) {
+    // Если не удалось подключиться к базе данных, сервер закрыт
+    $server_open_now = 0;
 }
 
-$server_open_now = "1";
+// Проверка статуса сервера
+if ($server_open_now == 0 && basename($_SERVER['PHP_SELF']) !== 'closed.php') {
+    header('Location: closed.php');
+    exit();
+}
 
 // Contacts (Change it!)
 $x_admin = "https://x.com/foxgalaxy23";
@@ -33,5 +37,4 @@ $th_admin = "https://t.me/lisisamp";
 $yt_admin = "https://www.youtube.com/@foxgalaxy23";
 $gh_admin = "https://github.com/foxgalaxy23g/MicroTok";
 $company_developer = "FoxGalaxy23";
-
 ?>
