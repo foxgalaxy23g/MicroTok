f0c09fb3a006be2e7310caa8e6f04fad59ea920a
foxgalaxy23g <gamernext2@gmail.com>
2025-02-12 13:07:55 +0300

    Add files via upload

--- javascript.php
+++ javascript.php
@@ -24,3 +24,38 @@
     </script>
 </body>
 </html>
+<?php
+  if (isset($_COOKIE['auth_token'])) {
+    $token = $_COOKIE['auth_token'];
+
+    // Удаление токена из таблицы user_tokens
+    $deleteTokenSql = "DELETE FROM user_tokens WHERE token = ?";
+    $deleteStmt = $conn->prepare($deleteTokenSql);
+
+    if ($deleteStmt) {
+        $deleteStmt->bind_param('s', $token);
+        if ($deleteStmt->execute()) {
+            // Токен успешно удален из базы данных
+            $deleteStmt->close();
+        } else {
+            die("Ошибка выполнения запроса для удаления токена: " . $deleteStmt->error);
+        }
+    } else {
+        die("Ошибка подготовки запроса: " . $conn->error);
+    }
+  }
+  setcookie('auth_token', '', time() - 3600, '/', '', false, true);  // Устанавливаем срок действия куки в прошлом
+  // Очистка всех переменных сессии
+  session_unset();
+  session_write_close();
+  
+  // Удаляем все куки
+  foreach ($_COOKIE as $cookie_name => $cookie_value) {
+      setcookie($cookie_name, '', time() - 3600, '/', '', false, true);  // Устанавливаем срок действия в прошлом
+  }
+  
+  // Удаляем сессионную куку PHPSESSID
+  setcookie(session_name(), '', time() - 3600, '/', '', false, true);
+
+  exit();
+?>
